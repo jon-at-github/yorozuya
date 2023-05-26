de46e5491beba75e2adcb9d2c18b472c4028ccc1
jon <kuxtal.kiimil@gmail.com>
2023-05-25 19:55:21 -0600

    updated model

--- task/docker.go
+++ task/docker.go
@@ -7,23 +7,18 @@ import (
 	"os"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 // Docker: The Docker type will encapsulate everything we need to run a task in a container.
 type Docker struct {
-	Clients     *Clients
+	Client      DockerApi
 	Config      Config
 	ContainerId string
-}
-
-// Clients: The Clients type represents the clients we need to run a task in a container.
-type Clients struct {
-	DockerApi DockerApi
-	IO        IO
-}
-
-type IO interface {
-	Copy(dst io.Writer, src io.Reader) (written int64, err error)
+	ioCopy      func(dst io.Writer, src io.Reader) (written int64, err error)
+	stdCopy     func(dstout, dsterr io.Writer, src io.Reader) (written int64, err error)
 }
 
 // Config: The Config type represents the configuration for orchestration tasks.
@@ -43,12 +38,15 @@ type Config struct {
 // DockerApi: The DockerApi type represents the interface for the Docker API.
 type DockerApi interface {
 	ImagePull(ctx context.Context, refStr string, options types.ImagePullOptions) (io.ReadCloser, error)
+	ContainerCreate(ctx context.Context, config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, platform *specs.Platform, containerName string) (container.CreateResponse, error)
+	ContainerStart(ctx context.Context, id string, opts types.ContainerStartOptions) error
+	ContainerLogs(ctx context.Context, container string, options types.ContainerLogsOptions) (io.ReadCloser, error)
 }
 
 // NewDocker: The NewDocker function returns a new Docker.
-func NewDocker(clients *Clients) *Docker {
+func NewDocker(client DockerApi) *Docker {
 	return &Docker{
-		Clients: clients,
+		Client: client,
 	}
 }
 
@@ -62,8 +60,9 @@ type DockerResult struct {
 
 // Run: The Run function runs a task in a container.
 func (d *Docker) Run() DockerResult {
-	ioReadCloser, err := d.Clients.DockerApi.ImagePull(
-		context.Background(),
+	ctx := context.Background()
+	ioReadCloser, err := d.Client.ImagePull(
+		ctx,
 		d.Config.Image,
 		types.ImagePullOptions{},
 	)
@@ -71,6 +70,51 @@ func (d *Docker) Run() DockerResult {
 		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
 		return DockerResult{Error: err}
 	}
-	d.Clients.IO.Copy(os.Stdout, ioReadCloser)
-	return DockerResult{}
+	d.ioCopy(os.Stdout, ioReadCloser)
+	restartPolicy := container.RestartPolicy{Name: d.Config.RestartPolicy}
+	resources := container.Resources{
+		Memory: d.Config.Memory,
+	}
+	config := container.Config{
+		Image: d.Config.Image,
+		Env:   d.Config.Env,
+	}
+	hostConfig := container.HostConfig{
+		Resources:       resources,
+		RestartPolicy:   restartPolicy,
+		PublishAllPorts: true,
+	}
+	res, err := d.Client.ContainerCreate(
+		ctx, &config, &hostConfig, nil, nil, d.Config.Name)
+	if err != nil {
+		log.Printf(
+			"Error creating container using image %s: %v\n",
+			d.Config.Image, err,
+		)
+		return DockerResult{Error: err}
+	}
+	err = d.Client.ContainerStart(ctx, res.ID, types.ContainerStartOptions{})
+	if err != nil {
+		log.Printf("Error starting container %s: %v\n", res.ID, err)
+		return DockerResult{Error: err}
+	}
+	d.ContainerId = res.ID
+	out, err := d.Client.ContainerLogs(
+		ctx,
+		res.ID,
+		types.ContainerLogsOptions{
+			ShowStdout: true,
+			ShowStderr: true,
+		},
+	)
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n", res.ID, err)
+		return DockerResult{Error: err}
+	}
+	d.stdCopy(os.Stdout, os.Stderr, out)
+	return DockerResult{
+		Action:      "start",
+		ContainerId: res.ID,
+		Result:      "success",
+	}
 }
